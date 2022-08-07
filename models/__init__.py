a1b77c35632898e6a08650abb524c06c51088c10
alienzyj <44811061+alienzyj@users.noreply.github.com>
2022-08-07 21:58:03 +0800

    Create __init__.py

---
+++ models/__init__.py
@@ -0,0 +1,7 @@
+from .my_classifier import Classifier
+from .my_genesis import Genesis2D
+from .my_unet import UNet
+from .my_unet_classifier import UNetClassifier
+from .my_ppos import PPos2D
+from .my_unet_classifier import UNetClassifierEncoder
+from .my_pcl import PCL2D
